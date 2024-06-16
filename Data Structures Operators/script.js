97d70a5004199cde023172ae687c35a90315ce43
Harshath Sundaram <harshathsundaram@gmail.com>
2024-06-16 21:42:19 +0530

    Short Circuiting

--- Data Structures Operators/script.js
+++ Data Structures Operators/script.js
@@ -48,8 +48,93 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
 };
 
+/*
+///////////////////////////////////////////////////////////////////////////
+// Short Circuiting (&& and ||)
+
+console.log('------------OR------------------------------');
+// use ANY data type, return ANY data type
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('------------------AND---------------------------');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+//example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinashc');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+//////////////////////////////////////////////////////////////////////////
+*/
+
+
+/*
+////////////////////////////////////////////////////////////////////////
+// Rest Pattern and parameters
+
+// Destructuring
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// Functions
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olivers', 'spinich');
+
+restaurant.orderPizza('mushrooms');
+
+///////////////////////////////////////////////////////////////////////
+*/
+
+/*
+/////////////////////////////////////////////////////////////////////
 // Spread Operator
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -92,8 +177,11 @@ console.log(newRestaurant);
 const restaurantCopy = {...restaurant}
 restaurantCopy.name = 'One Fine Day';
 console.log(restaurantCopy.name, restaurant.name);
+/////////////////////////////////////////////////////////////////////////
+*/
 
 /*
+/////////////////////////////////////////////////////////////////
 // Destructuring Objects
 restaurant.orderDelivery({
   time: '22:30',
@@ -134,10 +222,11 @@ const {
 } = openingHours;
 
 console.log(o, c);
-
+///////////////////////////////////////////////////////////////////
 */
 
 /* Destructring Arrays
+///////////////////////////////////////////////////////////////////////
 const arr = [2,3,4];
 const [a,b,c] = arr;
 console.log(a,b,c);
@@ -168,4 +257,5 @@ console.log(i, j, k);
 //Default values
 const[p=1,q=1,r=1] = [0,9];
 console.log(p, q, r);
+////////////////////////////////////////////////////////////////
 */
