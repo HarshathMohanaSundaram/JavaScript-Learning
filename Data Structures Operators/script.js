ad84ccf130d884c6a627ab8f00d58df969cf0a4c
Harshath Sundaram <harshathsundaram@gmail.com>
2024-06-16 21:55:07 +0530

    Logical Assignment Operator

--- Data Structures Operators/script.js
+++ Data Structures Operators/script.js
@@ -55,6 +55,43 @@ const restaurant = {
   },
 };
 
+
+/*
+////////////////////////////////////////////////////////////////////
+// Logical Assignment Operator
+const rest1 = {
+  name: 'Capri',
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Pizza',
+  owner: 'Basil'
+};
+
+
+// OR assignment Operator
+// rest1.numGuests =  rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||=10;
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+
+console.log(rest1);
+console.log(rest2);
+////////////////////////////////////////////////////////////////////////////
+*/
+
 /*
 ///////////////////////////////////////////////////////////////////////////
 // Nullish Coalescing Operator(??)
