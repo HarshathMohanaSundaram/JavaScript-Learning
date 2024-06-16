831c2311371dc426ef6a052a906719a47ccac00e
Harshath Sundaram <harshathsundaram@gmail.com>
2024-06-16 10:59:33 +0530

    More examole on this keyword

--- Background Works/script.js
+++ Background Works/script.js
@@ -143,24 +143,52 @@
 
 // calcAge(1991);
 
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+
+// jonas.calcAge();
+
+// const harsha = {
+//   year: 2017,
+// };
+
+// harsha.calcAge = jonas.calcAge;
+
+// harsha.calcAge();
+
+// const f = jonas.calcAge;
+
+// f();
+
+var firstName = "Harsha";
+
 const jonas = {
+  firstName: "Jonas",
   year: 1991,
   calcAge: function () {
     console.log(this);
     console.log(2037 - this.year);
+    const isMillenial = function(){
+      console.log(this.year >=1981 && this.year <=1996);
+    }
+
+    isMillenial();
+  },
+
+  // greet: () => {
+  //   console.log(this);
+  //   console.log(`Hey ${this.firstName}`); //arrow function does not have its own `this` it access it from parent scope
+  // },
+  greet: function(){
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
   },
 };
 
-jonas.calcAge();
-
-const harsha = {
-  year: 2017,
-};
-
-harsha.calcAge = jonas.calcAge;
-
-harsha.calcAge();
-
-const f = jonas.calcAge;
-
-f();
+jonas.greet();
+jonas.calcAge()
