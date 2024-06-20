304e892f5c459a49fa022618fc721182d6fcc1e1
Harshath Sundaram <harshathsundaram@gmail.com>
2024-06-20 23:44:12 +0530

    String method

--- Data Structures Operators/script.js
+++ Data Structures Operators/script.js
@@ -51,6 +51,24 @@ const restaurant = {
   },
 };
 
+/*
+//////////////////////////////////////////////////////////////////////////////////////
+// String Practice
+console.log(flights.split('+'));
+
+const getCode = str => str.slice(0,3).toUpperCase()
+
+for(const flight of flights.split('+')){
+  const[type, from, to, time] = flight.split(';');
+  
+  const output = `${type.startsWith('_Delayed') ? ':( ':''}${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} (${time.replace(':','h')})`.padStart(36);
+
+  console.log(output);
+
+}
+//////////////////////////////////////////////////////////////////////////////////////
+*/
+
 /////////////////////////////////////////////////////////////////////////////////////
 // Strings
 
