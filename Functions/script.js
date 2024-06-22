5b5a3805bb9043f5f9a99b1e328b80a26c42558d
Harshath Sundaram <harshathsundaram@gmail.com>
2024-06-22 12:43:25 +0530

    Setting `this` Keyword Explicitly

--- Functions/script.js
+++ Functions/script.js
@@ -132,7 +132,7 @@ const book = lufthansa.book;
 // Not work
 //book(23, 'Sarah Williams');
 
-/* Call and Apply for setting `this` keywor explictly
+/* Call, Apply, and Bind Method for setting `this` keyword explictly
 // Call Method
 book.call(eurowings, 23, 'Sarah Williams');
 console.log(eurowings);
@@ -149,9 +149,66 @@ const swiss = {
 book.call(swiss, 78, 'Johan');
 console.log(swiss);
 
-
 // Apply method
 const flightData = [583, 'Francis George'];
 book.apply(swiss, flightData);
 console.log(swiss);
+
+//  Bind Method
+// book.call(eurowings, 23, 'Sarah Williams');
+const bookEw = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookSWA = book.bind(swiss);
+bookEw(28, 'Hawkings');
+console.log(eurowings);
+bookLH(34, 'Daniel');
+console.log(lufthansa);
+bookSWA(2134, 'Sophia');
+console.log(swiss);
+
+const bookEw23 = book.bind(eurowings, 23);
+bookEw23('Harshath');
+bookEw23('George');
+
+// with Event Listeners
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * (rate / 100);
+
+console.log(addTax(10, 200));
+
+const addVAT = addTax.bind(null, 23);
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const taxCalculator = (VAT) => (value) => VAT(value);
+
+const vatCalculator = taxCalculator(addVAT);
+
+console.log(vatCalculator(100));
+
+const addTaxRate = function(rate) {
+  return function(value) {
+    return value + (value * (rate/100))
+  }
+}
+
+const addVAT2 = addTaxRate(23);
+console.log(addVAT2(199));
+console.log(addVAT2(100));
+
 */
