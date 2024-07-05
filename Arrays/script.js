68a386103317d04506ec67d57b2254d68f9546c3
Harshath Sundaram <harshathsundaram@gmail.com>
2024-07-05 16:35:10 +0530

    Sorting Arrays

--- Arrays/script.js
+++ Arrays/script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a > b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `<div class="movements__row">
@@ -209,6 +211,13 @@ btnClose.addEventListener('click', function (e) {
   }
 
   inputClosePin.value = inputCloseUsername.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
@@ -503,4 +512,28 @@ const overalBalance = accounts
   .flatMap(acc => acc.movements)
   .reduce((tot, mov) => tot + mov, 0);
 console.log(overalBalance);
+*/
+
+/*
+// Sorting Arrays
+
+// Strings
+const owners = ['Jonas', 'Deepa', 'Zack', 'Avana'];
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
+console.log(movements);
+
+movements.sort((a, b) => a > b);
+console.log(movements);
+
+movements.sort((a, b) => a < b);
+console.log(movements);
+
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+movements.sort((a, b) => b - a);
+console.log(movements);
 */
