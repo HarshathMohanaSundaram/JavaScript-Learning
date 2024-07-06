8888996549b0e43db795a72fc756aa2609920967
Harshath Sundaram <harshathsundaram@gmail.com>
2024-07-06 15:28:00 +0530

    Numeric Separators

--- Number-Date-Timer/script.js
+++ Number-Date-Timer/script.js
@@ -343,3 +343,23 @@ labelBalance.addEventListener('click', () => {
   });
 });
 */
+
+/*
+// Numeric Separators
+
+//287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;// 1500
+const transferFee2 = 1_500;// 1500
+
+const PI = 3.14_15
+console.log(PI); 
+
+console.log(Number('230_000'));// NaN
+console.log(parseInt('230_000'));// 230
+*/
