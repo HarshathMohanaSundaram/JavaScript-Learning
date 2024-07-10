ed0a6de5b7de47b99ed54e89cafe88c1b51b8246
Harshath Sundaram <harshathsundaram@gmail.com>
2024-07-10 18:51:08 +0530

    ``Select``,`` create``, ``Delete`` Elements

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -19,8 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-
-btnsOpenModal.forEach((btnOpenModal) => btnOpenModal.addEventListener('click', openModal));
+btnsOpenModal.forEach(btnOpenModal =>
+  btnOpenModal.addEventListener('click', openModal)
+);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -30,3 +31,43 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////////////////////////////////
+//////
+
+// Select, Create and Delete Elements
+console.log(document.documentElement); // Select entire elements
+console.log(document.head); // Select head tag
+console.log(document.body); // Select body tag
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and Inserting Elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improve functionality and analystics.';
+message.innerHTML =
+  'We use cookies for improve functionality and analystics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true))
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+
+    // alternate
+    //message.parentElement.removeChild(message)
+  });
