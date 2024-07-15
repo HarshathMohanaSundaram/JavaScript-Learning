79799c2a6f3e9de7032e7f1f7b09d0614c21bc27
Harshath Sundaram <harshathsundaram@gmail.com>
2024-07-15 09:54:22 +0530

    smooth scrooling

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -107,3 +107,37 @@ logo.classList.remove('c');// can add multiples
 logo.classList.toggle('c');
 logo.classList.contains('c')
 */
+
+// Smooth Scrolling
+const btnScroll = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScroll.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current Scroll (X/Y)', window.scrollX, scrollY);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // scrolling
+  
+  // old way
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  // Modern Way
+  section1.scrollIntoView({behavior: 'smooth'})
+});
