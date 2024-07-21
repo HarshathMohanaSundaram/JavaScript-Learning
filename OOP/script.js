c1e7a5fd99bfdc2ec61c092fc4023f3bc8d85a9b
Harshath Sundaram <harshathsundaram@gmail.com>
2024-07-21 19:33:34 +0530

    Chaining Method

--- OOP/script.js
+++ OOP/script.js
@@ -403,10 +403,12 @@ class Account {
 
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+    return this;
   }
 
   // Private Method
@@ -424,6 +426,7 @@ class Account {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+    return this;
   }
 
   getMovements() {
@@ -439,4 +442,10 @@ const acc1 = new Account('Harshath', 'INR', 1111);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+Account.helper();
+
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
