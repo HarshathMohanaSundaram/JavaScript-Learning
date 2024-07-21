0feea5f5d3c0fb40f93b10c0b19c79731e728586
Harshath Sundaram <harshathsundaram@gmail.com>
2024-07-21 18:44:10 +0530

    Coding Challenge 3

--- OOP/script.js
+++ OOP/script.js
@@ -214,7 +214,7 @@ console.log(car.speedUS);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //// Inheritance Between "Classes": Constructor
-
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -250,5 +250,48 @@ console.log(Student.prototype.constructor);
 
 Student.prototype.constructor = Student;
 console.log(Student.prototype.constructor);
+*/
+
+/*
+// Coding Challenge 3
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/hr`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/hr`);
+};
+
+const EV = function(make, speed, charge){
+  Car.call(this, make, speed)
+  this.charge = charge
+}
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function(chargeTo){
+  this.charge = chargeTo;
+}
+
+EV.prototype.accelerate = function(){
+  this.speed += 20;
+  this.charge--;
+  console.log(`${this.make} is going at ${this.speed} km/hr with a charge of ${this.charge}`);
+}
+const tesla = new EV('Tesla', 120, 23);
+
+console.log(tesla);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+tesla.accelerate();
+*/
 
 
